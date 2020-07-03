2d36cc6c267b87a59d38486147da77a3ce5db290
kanchana849849 <ita5915@ccollege.ac.th>
2020-07-03 12:49:44 +0700

    start

---
+++ pages/activity/flag.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        กิจกรรมหน้าเสาธง
+        <br>
+            <nuxt-link to="/menu"> 
+             <v-btn depressed small color="primary">กลับหน้าหลัก</v-btn>  
+            </nuxt-link>
+    </div>
+</template>
---
+++ pages/activity/homeroom.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        กิจกรรมโฮมรูม
+        <br>
+        <div class="my-2">
+        
+     
+            <nuxt-link to="/menu"> 
+           <v-btn depressed small color="primary">กลับหน้าหลัก</v-btn> 
+            </nuxt-link>
+             </div>
+    </div>
+
+</template>
