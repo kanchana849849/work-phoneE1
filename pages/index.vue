0c3be236cb2476d36b219d5455b4f531ea8b55bc
kanchana849849 <ita5915@ccollege.ac.th>
2020-07-08 15:43:03 +0700

    start

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,14 @@
     <br>
             <nuxt-link to="/menu"> 
              <v-btn depressed small color="primary">หน้าเมนูกิจกรรม</v-btn>  
+             
             </nuxt-link>
+             <nuxt-link to="/ex01"> 
+             <v-btn depressed small color="primary">กลับหน้าล็อคอิน</v-btn>  
+             
+            </nuxt-link>
+            
     </div>
-  </div>
+     
+  </template>
 </template>
