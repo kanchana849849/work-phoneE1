03a494dd7d2f145d906ffbf8a7973ed9c67d163e
kanchana849849 <ita5915@ccollege.ac.th>
2020-07-08 15:34:37 +0700

    start

---
+++ pages/ex01.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Hello: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+    <h2>name:<input type="text" :value="name"></h2>
+    <input type="text" :v-model="name">
+    <div><button @click="doSave">SAVE</button></div>
+    <div><button @click="doMenu">MAIN MENU</button></div>
+    <div><button @click="doHome">HOME</button></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Kanchana Phonsena",
+      age: 20,
+     }
+  },
+methods: {
+    doSave() {
+      console.log("DO SAVE");
+    },
+    doMenu() {
+      this.$router.push("/menu");
+    },
+    doHome() {
+      this.$router.push("/");
+    }
+  }
+}
+</script>
